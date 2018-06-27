b3ce3ceb422d0e2d7812f4c95cda49a0922003d1
Abdulla thanseeh <thanseehabdulla@gmail.com>
2018-06-27 12:42:06 +0530

    ecommerce login updated with carts

--- keystone.js
+++ keystone.js
@@ -29,7 +29,7 @@ keystone.init({
 	}).engine,
 
 	'emails': 'templates/emails',
-    'cloudinary config' : 'cloudinary://a884896682467366:Jf-YGhj_z4Nyq_lt7RnGdCXr6Xg@dwmpmeasl',
+    										'cloudinary config': 'cloudinary://a884896682467366:Jf-YGhj_z4Nyq_lt7RnGdCXr6Xg@dwmpmeasl',
 	'auto update': true,
 	'session': true,
 	'auth': true,
@@ -50,8 +50,7 @@ keystone.set('locals', {
 });
 
 
-keystone.set('cloudinary config', 'cloudinary://884896682467366:Jf-YGhj_z4Nyq_lt7RnGdCXr6Xg@dwmpmeasl' );
-
+keystone.set('cloudinary config', 'cloudinary://884896682467366:Jf-YGhj_z4Nyq_lt7RnGdCXr6Xg@dwmpmeasl');
 
 
 // Load your project's Routes
@@ -60,6 +59,9 @@ keystone.set('routes', require('./routes'));
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
 	users: 'users',
+	cart: 'Cart',
+	product: 'Product',
+	order: 'Order',
 });
 
 // Start Keystone to connect to your database and initialise the web server
